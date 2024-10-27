6dda2b4970a6a7275263e6e71a128acc73954d96
DeniS <denistaran482@gmail.com>
2024-10-27 18:45:58 +0200

    add task-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import article from './data/article.json';
 import stats from './data/stats.json';
+import forbes from './data/forbes.json';
 
 import {
   Section,
@@ -32,7 +33,7 @@ export const App = () => {
         <Statistics title="Main Statistics" stats={stats} />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbes} />
 
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
